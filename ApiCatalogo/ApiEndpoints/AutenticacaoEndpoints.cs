04cd718214c1609a9a6369ab511b40723fd469d8
Samuelpj <samueljacobsen@alunos.utfpr.edu.br>
2023-01-19 18:04:08 -0300

    Projeto Concluido

---
+++ ApiCatalogo/ApiEndpoints/AutenticacaoEndpoints.cs
@@ -0,0 +1,39 @@
+﻿using ApiCatalogo.Models;
+using ApiCatalogo.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Runtime.CompilerServices;
+
+namespace ApiCatalogo.ApiEndpoints
+{
+    public static class AutenticacaoEndpoints
+    {
+        public static void MapAutenticacaoEndpoints(this WebApplication app)
+        {
+
+            //endpoint para login
+            app.MapPost("/login", [AllowAnonymous] (UserModel userModel, 
+                ITokenService tokenService) =>
+            {
+                if (userModel == null)
+                {
+                    return Results.BadRequest("Login Invalido");
+                }
+                if (userModel.UserName == "Samuel" && userModel.Password == "numsey#123")
+                {
+                    var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+                        app.Configuration["Jwt:Issuer"],
+                        app.Configuration["Jwt:Audience"],
+                        userModel);
+                    return Results.Ok(new { token = tokenString });
+                }
+                else
+                {
+                    return Results.BadRequest("Login Invalido");
+                }
+            }).Produces(StatusCodes.Status400BadRequest)
+                          .Produces(StatusCodes.Status200OK)
+                          .WithName("Login")
+                          .WithTags("Autenticacao");
+        }
+    }
+}
